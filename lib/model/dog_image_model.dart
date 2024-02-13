af4940868b74f620c2f0c344d246311a916e63a4
githmipandigama <152261759+githmipandigama@users.noreply.github.com>
2024-02-13 15:01:53 +0530

    create the directiries

---
+++ lib/model/dog_image_model.dart
@@ -0,0 +1,18 @@
+class DogImageModel {
+  String? message;
+  String? status;
+
+  DogImageModel({this.message, this.status});
+
+  DogImageModel.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    status = json['status'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    data['status'] = this.status;
+    return data;
+  }
+}
