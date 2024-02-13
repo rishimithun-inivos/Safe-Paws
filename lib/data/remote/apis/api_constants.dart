04da9a5088df42fa1d358be07193e4b23c978483
githmipandigama <152261759+githmipandigama@users.noreply.github.com>
2024-02-13 14:06:48 +0530

    start making API call

---
+++ lib/data/remote/apis/api_constants.dart
@@ -0,0 +1,3 @@
+class ApiCalls{
+  static final dogApiBaseUrl = 'https://dog.ceo';
+}
---
+++ lib/data/remote/network/baseapiservice.dart
@@ -0,0 +1,5 @@
+abstract class BaseApiService {
+  Future<dynamic> getGetApiResponse(
+    String url,
+  );
+}
